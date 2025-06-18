da2504a44869970323271279adc013f012fad1dc
winterkid <137682088+winterkid2000@users.noreply.github.com>
2025-06-19 08:37:59 +0900

    Update tester.py

--- tester.py
+++ tester.py
@@ -12,7 +12,7 @@ def load_dicom_image(dicom_path):
     reader.SetFileNames(dicom_files)
     return reader.Execute()
 
-def get_mask_and_ref(dicom_dir, total_rt_path):
+def get_mask_and_ref(dicom_path, total_rt_path):
     ref_image = load_dicom_image(dicom_dir)
     rt_total = RTStructBuilder.create_from(dicom_dir, total_rt_path)
     
@@ -21,17 +21,17 @@ def get_mask_and_ref(dicom_dir, total_rt_path):
     return mask_total, ref_image
   
 def tester(mask_total, ref_image):
-  mask_coord = mask_total.shape
-  ref_image_coord = ref_image.GetSize()
-  return mask_coord, ref_image_coord
+    mask_coord = mask_total.shape
+    ref_image_coord = ref_image.GetSize()
+    return mask_coord, ref_image_coord
 
 def main():
-print('테스트')
-dicom_path = r''
-total_rt_path = r''
-mask_total, ref_image = get_mask_and_ref(dicom_dir, total_rt_path)
-mask_coord, ref_image_coord = tester(mask_total, ref_image)
-print(f'마스크: {mask_coord}, CT: {ref_image_coord}')
+    print('테스트')
+    dicom_path = r''
+    total_rt_path = r''
+    mask_total, ref_image = get_mask_and_ref(dicom_dir, total_rt_path)
+    mask_coord, ref_image_coord = tester(mask_total, ref_image)
+    print(f'마스크: {mask_coord}, CT: {ref_image_coord}')
 
 if __name__ == "__main__":
   main()
