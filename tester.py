f3196d2ecf41fa7bd85c142ead2161059484cbba
winterkid <137682088+winterkid2000@users.noreply.github.com>
2025-06-19 12:40:38 +0900

    Update tester.py

--- tester.py
+++ tester.py
@@ -20,7 +20,7 @@ def get_mask_and_ref(dicom_path, total_rt_path):
     
     return mask_total, ref_image
   
-def tester(mask_total, ref_image):
+def tester1(mask_total, ref_image):
     mask_coord = mask_total.shape
     ref_image_coord = ref_image.GetSize()
     return mask_coord, ref_image_coord
@@ -39,7 +39,7 @@ def main():
     dicom_path = r''
     total_rt_path = r''
     mask_total, ref_image = get_mask_and_ref(dicom_path, total_rt_path)
-    mask_coord, ref_image_coord = tester(mask_total, ref_image)
+    mask_coord, ref_image_coord = tester1(mask_total, ref_image)
     print(f'마스크: {mask_coord}, CT: {ref_image_coord}')
     yxz, xyz = tester2(mask_total, ref_image)
     print(f'yxz: {yxz}, xyz: {xyz}') 
