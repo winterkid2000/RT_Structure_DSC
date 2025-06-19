5b4c2aaef3b16e5acc9455a55c25f4a3895bfe51
winterkid <137682088+winterkid2000@users.noreply.github.com>
2025-06-19 09:02:07 +0900

    Update tester.py

--- tester.py
+++ tester.py
@@ -13,7 +13,7 @@ def load_dicom_image(dicom_path):
     return reader.Execute()
 
 def get_mask_and_ref(dicom_path, total_rt_path):
-    ref_image = load_dicom_image(dicom_dir)
+    ref_image = load_dicom_image(dicom_path)
     rt_total = RTStructBuilder.create_from(dicom_dir, total_rt_path)
     
     mask_total = rt_total.get_mask_by_name("Pancreas")
@@ -24,6 +24,15 @@ def tester(mask_total, ref_image):
     mask_coord = mask_total.shape
     ref_image_coord = ref_image.GetSize()
     return mask_coord, ref_image_coord
+def tester2(mask_total, ref_image): ##roi mask가 yxz인지 xyz인지 확인, ref_image의 좌표가 xyz라는 가정
+    sitk1 = sitk.GetImageFromArray(np.transpose(mask_total, (2, 0, 1)).astype(np.uint8))
+    sitk1.CopyInformation(ref_image) ##yxz
+    yxz = sitk1.GetSize()
+
+    sitk2 = sitk.GetImageFromArray(np.transpose(mask_total, (2, 1, 0)).astype(np.uint8))
+    sitk2.CopyInformation(ref_image) ##xyz
+    xyz = sitk2.GetSize()
+    return yxz, xyz
 
 def main():
     print('테스트')
@@ -32,6 +41,8 @@ def main():
     mask_total, ref_image = get_mask_and_ref(dicom_dir, total_rt_path)
     mask_coord, ref_image_coord = tester(mask_total, ref_image)
     print(f'마스크: {mask_coord}, CT: {ref_image_coord}')
+    yxz, xyz = tester2(mask_total, ref_image)
+    print(f'yxz: {yxz}, xyz: {xyz}') 
 
 if __name__ == "__main__":
   main()
